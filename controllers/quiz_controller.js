96bffe82c1bf7bcdf85d78aca1d4fec89c6fdeb1
Luis Moyano <luismoyanorufo@gmail.com>
2015-06-21 21:05:34 +0200

    Cambios del tema 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -32,13 +32,65 @@ exports.index = function(req,res){
         console.log("Va a devolver todos los quizzes");
 
         models.Quiz.findAll().success(function(quizzes){
-            res.render('quizes/index',{quizzes:quizzes})
+            res.render('quizes/index',{quizzes:quizzes});
         });
     } 
 }
 
 exports.show = function(req,res){
     res.render('quizes/question',{quiz:req.quiz});
+}
+
+exports.new = function(req,res){
+
+    var quiz = models.Quiz.build(
+        {pregunta:"",respuesta:""}
+    );
+
+    res.render('quizes/new',{quiz:quiz});
+}
+
+exports.create = function(req,res){
+
+    var quiz = models.Quiz.build(req.body.quiz);
+    console.log(req.body);
+
+    /*if(err){
+        res.render('quizes.new',{quiz:quiz,errors:err.errors});
+    }
+    else{*/
+        quiz.save({fields:["pregunta","respuesta"]}).then(function(){
+            res.redirect("/quizes");
+        });
+    //}
+
+    /*quiz.validate().then(function(err){
+        
+    });*/
+}
+
+exports.edit = function(req,res){
+
+    var quiz = req.quiz;
+    res.render('quizes/edit',{quiz:quiz});
+}
+
+exports.update = function(req,res){
+
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+    
+    // TODO: FAlta validar.
+    req.quiz.save({fields:["pregunta","respuesta"]}).then(function(){
+        res.redirect("/quizes");
+    });
+}
+
+exports.destroy = function(req,res){
+
+    req.quiz.destroy().then(function(){
+        res.redirect("/quizes");
+    });
 }
 
 exports.answer = function(req,res){
